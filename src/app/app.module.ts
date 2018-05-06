218fde1b0bb88bcdb38f163a6801259414a20c31
Jack Grier <jgrier93@gmail.com>
2018-05-06 11:13:31 +0100

    Adding support for ng2-carousel-3d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Ng2Carousel3dModule } from 'ng2-carousel-3d';
 
 import { AppComponent } from './app.component';
 import { BodyComponent} from './body/body.component';
+
+import { Ng2Carousel3dModule } from 'ng2-carousel-3d';
 
 @NgModule({
   declarations: [
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,34 +1,86 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent implements OnInit {
+export class BodyComponent {
 
   @ViewChild('carousel') carousel: any;
-  slides: Array<Object> = [];
+
+  slides: Array<Object> = [
+    {
+      'html': '<div class="card-deck mb-1 text-center">\n' +
+      '      <div class="card mb-4 box-shadow">\n' +
+      '        <div class="card-header">\n' +
+      '          <h4 class="my-0 font-weight-normal">Github</h4>\n' +
+      '        </div>\n' +
+      '        <div class="card-body">\n' +
+      '          <ul class="list-unstyled mt-3 mb-4">\n' +
+      '            <li>See my projects on Github</li>\n' +
+      '            <div class="github-card"\n' +
+      '                 data-github="JackGrier"\n' +
+      '                 data-width="400"\n' +
+      '                 data-height=""\n' +
+      '                 data-theme="default"></div>\n' +
+      '            <script src="https://cdn.jsdelivr.net/gh/lepture/github-cards@latest/jsdelivr/widget.js"></script>\n' +
+      '          </ul>\n' +
+      '        </div>\n' +
+      '      </div>\n' +
+      '    </div>'
+    },
+    {
+      'html': '<div class="card-deck mb-1 text-center">\n' +
+      '        <div class="card mb-4 box-shadow">\n' +
+      '          <div class="card-header">\n' +
+      '            <h4 class="my-0 font-weight-normal">LinkedIn</h4>\n' +
+      '          </div>\n' +
+      '          <div class="card-body">\n' +
+      '            <ul class="list-unstyled mt-3 mb-4">\n' +
+      '              <li>Full stack engineer</li>\n' +
+      '              <li>Experience with:</li>\n' +
+      '              <li>Angular</li>\n' +
+      '              <li>Angular Js</li>\n' +
+      '              <li>Java</li>\n' +
+      '              <li>Spring</li>\n' +
+      '              <li>Hibernate</li>\n' +
+      '              <li>For more <a href="https://www.linkedin.com/in/jack-grier-223a1a57/">Check out my profile</a></li>\n' +
+      '              <li><img src="../../assets/CFA_badge.png" alt="CFA Certification Badge"></li>\n' +
+      '            </ul>\n' +
+      '          </div>\n' +
+      '        </div>\n' +
+      '      </div>'
+    },
+    {
+      'html': '      <div class="card-deck mb-1 text-center">\n' +
+      '        <div class="card mb-4 box-shadow">\n' +
+      '          <div class="card-header">\n' +
+      '            <h4 class="my-0 font-weight-normal">Twitter</h4>\n' +
+      '          </div>\n' +
+      '          <div class="card-body">\n' +
+      '            <a href="https://twitter.com/jgrier93?ref_src=twsrc%5Etfw" class="twitter-follow-button"\n' +
+      '               data-show-count="false">Follow @jgrier93</a>\n' +
+      '          </div>\n' +
+      '        </div>\n' +
+      '      </div>'
+    }
+  ];
+
   options: Object = {
     clicking: true,
     sourceProp: 'src',
-    visible: 7,
+    visible: 3,
     perspective: 1,
     startSlide: 0,
-    border: 3,
+    border: 2,
     dir: 'ltr',
-    width: 360,
-    height: 270,
+    width: 600,
+    height: 600,
     space: 220,
     autoRotationSpeed: 5000,
     loop: true
   };
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
 
   slideClicked(index) {
     this.carousel.slideClicked(index);
